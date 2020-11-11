a31cf8e210a1a36877d9181dbb28f181fc59c0e3
Daniel Michael <itzdanielmichael@gmail.com>
2020-11-11 14:45:17 -0500

    install eslint

--- server/src/index.ts
+++ server/src/index.ts
@@ -14,3 +14,6 @@ console.log(`[app]: http://localhost:${port}`)
 //npm install typescript ts-node -D
 // ts-node will help me run typescript code directly from the terminal.
 // next I have to make a tsconfig.json file
+
+// for eslint support
+// npm install -D eslint @typescript-eslint/parser @typescript-eslint/eslint-plugin
