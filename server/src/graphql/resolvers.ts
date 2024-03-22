60615f7def0324ebc09f2650f6b210ea8bdbbc4f
daniel <itzdanielmichael@gmail.com>
2024-03-22 17:59:03 -0400

    add create listing

--- server/src/graphql/resolvers.ts
+++ server/src/graphql/resolvers.ts
@@ -1,21 +1,34 @@
-import { IResolvers } from "apollo-server-express";
-import { listings } from "../listings";
+import { IResolvers } from 'apollo-server-express';
+import { Listing, listings } from '../listings';
 
 export const resolvers: IResolvers = {
   Query: {
-    listings: () => {
+    listings: (): Listing[] => {
       return listings;
     },
   },
   Mutation: {
-    deleteListing: (_root: undefined, { id }: { id: string }) => {
+    createListing: (
+      _root: undefined,
+      { input }: { input: Partial<Listing> }
+    ): Listing | Error => {
+      const id = String(listings.length + 1);
+      const newListing = { id, ...input } as Listing;
+      listings.push(newListing);
+      return newListing;
+    },
+
+    deleteListing: (
+      _root: undefined,
+      { id }: { id: string }
+    ): Listing | Error => {
       for (let i = 0; i < listings.length; i++) {
         if (listings[i].id === id) {
           return listings.splice(i, 1)[0];
         }
       }
 
-      throw new Error("failed to deleted listing");
+      throw new Error('failed to deleted listing');
     },
   },
 };
--- server/src/graphql/typeDefs.ts
+++ server/src/graphql/typeDefs.ts
@@ -1,4 +1,4 @@
-import { gql } from "apollo-server-express";
+import { gql } from 'apollo-server-express';
 
 export const typeDefs = gql`
   type Listing {
@@ -13,11 +13,23 @@ export const typeDefs = gql`
     rating: Int!
   }
 
+  input ListingInput {
+    title: String!
+    image: String!
+    address: String!
+    price: Int!
+    numOfGuests: Int = 0
+    numOfBeds: Int = 0
+    numOfBaths: Int = 0
+    rating: Int = 0
+  }
+
   type Query {
     listings: [Listing!]!
   }
 
   type Mutation {
     deleteListing(id: ID!): Listing!
+    createListing(input: ListingInput!): Listing!
   }
 `;
